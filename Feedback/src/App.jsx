0f8a7cd050ef2a36d6d3c7849277276df6392528
Somgester <dhruvratmele13@gmail.com>
2024-03-10 10:55:07 +0530

    another commit

--- Feedback/src/App.jsx
+++ Feedback/src/App.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
 const Styles = styled.div`
-    body {
-      font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
-      overflow-x: hidden;
-      overflow-y: auto;
-    }
+body {
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  overflow-x: hidden;
+  overflow-y: auto;
+  height: 100vh;
+  width: 100vw;
+}
 
   header {
     background-color: #f2f2f2;
